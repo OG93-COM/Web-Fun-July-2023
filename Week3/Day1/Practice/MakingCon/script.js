64b32eaef45636f739d81aff58dd0471a0c12b25
Oussama Galai <oucema03@gmail.com>
2023-07-27 19:03:09 +0100

    Making Conn

--- Week3/Day1/Practice/MakingCon/script.js
+++ Week3/Day1/Practice/MakingCon/script.js
@@ -1,1 +1,26 @@
 console.log("page loaded...");
+
+var NumbRequest = document.querySelector(".numbrequests")
+var numbConn = document.querySelector(".numbCon")
+
+
+function accept(profil) {
+    let element = document.querySelector(profil)
+    element.remove()
+    NumbRequest.innerText--
+    numbConn.innerText++
+    console.log(numbConn)
+}
+
+function dontaccept(profil) {
+    let element = document.querySelector(profil)
+    element.remove()
+    NumbRequest.innerText--
+    console.log(numbConn)
+}
+
+var newName = document.querySelector(".cardusername")
+
+function editprofile() {
+    newName.innerText = "New Name"
+}
